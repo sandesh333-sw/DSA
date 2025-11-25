084e4af6521d3d483ce66af1a2962b9db350108c
Sandesh Pulami <sandeshpulami333@gmail.com>
2025-11-25 12:38:35 +0000

    DP

--- DP Part2/Knapsack.java
+++ DP Part2/Knapsack.java
@@ -42,6 +42,42 @@ public class Knapsack {
         }
     }
 
+    //tabulation
+    public static int knapSackTab(int[] val, int[] wt, int W){
+        int n = val.length;
+        int[][] dp = new int[n+1][W+1];
+        for(int i=0; i<dp.length; i++){
+            dp[i][0] = 0;
+        }
+
+        for(int j=0; j<dp[0].length; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<W+1; j++){
+                int v = val[i-1];
+                int w = wt[i-1];
+
+                //valid condition
+                if(w <= j){
+                    //include 
+                    int incProfit = v + dp[i-1][j-w];
+
+                    //exclude
+                    int excProfit = dp[i-1][j];
+
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                } else {
+                    int escProfit = dp[i-1][j];
+                    dp[i][j] = escProfit;
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+
     public static void main(String[] args) {
         int[] val = {15, 14, 10, 45, 30};
         int[] wt = {2, 5, 1, 3, 4};
